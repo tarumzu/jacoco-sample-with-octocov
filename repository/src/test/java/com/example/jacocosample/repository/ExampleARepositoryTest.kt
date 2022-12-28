62fdfa0c56d5a46ded0f0a82669f6a13d3296ca1
tarumzu <tarumzu@gmail.com>
2022-12-28 13:22:42 +0900

    add ExampleARepositoryTest

---
+++ repository/src/test/java/com/example/jacocosample/repository/ExampleARepositoryTest.kt
@@ -0,0 +1,12 @@
+package com.example.jacocosample.repository
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleARepositoryTest {
+
+    @Test
+    fun get_isCorrect() {
+        Assert.assertEquals("test", ExampleARepository().get())
+    }
+}
